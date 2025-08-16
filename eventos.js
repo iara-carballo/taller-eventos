e391caed19f99d512e31bda53b9fa1bbe6a16614
Iara Carballo <iaracarballo09@gmail.com>
2025-08-15 23:23:12 -0300

    Hola! Soy el div
    8

---
+++ eventos.js
@@ -0,0 +1,7 @@
+// Seleccionamos el div por su id
+const miDiv = document.getElementById("contenedor");
+
+// Agregamos un manejador de evento para el click
+miDiv.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
